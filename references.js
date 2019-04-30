b503ab2f87fa7feec5e9591da3ee3c651a85357b
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-04-30 16:28:43 -0400

    modularization testing

---
+++ references.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo_2", {useNewUrlParser: true});
+
+var Post = require("./models/post");
+var User = require("./models/user");
+
+Post.create({
+  title: "How to cook a burger part six",
+  content: "boolalalalalalalalalalalalalalalal"
+}, function(err, post){
+   User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else  {
+      foundUser.posts.push(post);
+      foundUser.save(function(err, data){
+        if(err){
+          console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
+    }
+  });
+});
+//
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(user);
+//   }
+// });
